0f9c10bd73eb31eccf0bb858d33380b4aac7ea06
Raisto <ShenYuanLuo@gmail.com>
2014-10-28 16:50:46 +0800

    I am sever branch

--- Test.c
+++ Test.c
@@ -12,6 +12,7 @@
 main ( int argc, char *argv[] )
 {
     printf("I am master branch !\n");
+    printf("I am sever branch !\n");
 
     return EXIT_SUCCESS;
 }       /* ----------  end of function main  ---------- */
