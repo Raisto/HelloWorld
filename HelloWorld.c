56cf9f699ad2228838146e906f9fa4f96cf407d0
Raisto <ShenYuanLuo@gmail.com>
2014-10-28 11:02:11 +0800

    create HelloWorld.c file

---
+++ HelloWorld.c
@@ -0,0 +1,20 @@
+
+#include	<stdlib.h>
+#include	<stdio.h>
+
+/* 
+ * ===  FUNCTION  ==================================================
+ *         Name:  main
+ *  Description:  
+ * =================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+{
+    printf("Hello World !\n");
+
+    return EXIT_SUCCESS;
+}       /* ----------  end of function main  ---------- */
+
+
+
