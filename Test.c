3434c646d4adf9c6429703ffe842f469c2a12695
Raisto <ShenYuanLuo@gmail.com>
2014-10-28 16:43:25 +0800

    I am master branch

---
+++ Test.c
@@ -0,0 +1,20 @@
+
+#include	<stdlib.h>
+#include	<stdio.h>
+
+/* 
+ * ===  FUNCTION  ==================================================
+ *         Name:  main
+ *  Description:  
+ * =================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+{
+    printf("I am master branch !\n");
+
+    return EXIT_SUCCESS;
+}       /* ----------  end of function main  ---------- */
+
+
+
